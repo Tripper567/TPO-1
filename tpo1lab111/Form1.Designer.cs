7528b93a62fa7fa191127c297f0a748542e2b9a1
Дмитрий <@DESKTOP-GT9VIAG>
2022-09-21 00:45:33 +0500

    Добавьте файлы проекта.

---
+++ tpo1lab111/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace tpo1lab111
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 233);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 364);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(191, 12);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(378, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Начало интегрирования";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Конец интегрирования";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Точность";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 364);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Интеграл";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 327);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Шаг";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::tpo1lab111.Properties.Resources.f;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(585, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(157, 300);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 386);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Метод трапеции";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ tpo1lab111/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpo1lab111
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, h, n = 10, res, res2, res1 = 0, resCount;
+            double x , eps, an = 0;
+
+            
+            try
+            {
+                a = Convert.ToDouble(textBox1.Text);
+                b = Convert.ToDouble(textBox2.Text);
+                eps = Convert.ToDouble(textBox3.Text);
+                int count = GetDecimalDigitsCount(eps);
+
+                if (Math.Abs(a) == b || Math.Abs(eps) > 1)
+                { 
+                        textBox1.Clear();
+                        textBox2.Clear();
+                        textBox3.Clear();    
+                        MessageBox.Show("Вы ввели парные числа интегрирования или указана неверная точность");
+                }
+
+                else
+                {
+                    richTextBox1.Clear();
+                    do
+                    {
+                        h = (b - Math.Abs(a)) / n;
+                        label1.Text = Convert.ToString(Math.Round(h, count));
+                        x = a; res = 0;
+                        while (x + h <= b)
+                        {
+                            if (x < 0 || x > 160000)
+                            {
+                                richTextBox1.AppendText("Число в не ОДЗ" + x + '\n');
+                            }
+                            else
+                            {
+
+                                res += h * Math.Sqrt(400 - Math.Sqrt(x));
+                                resCount = h * Math.Sqrt(400 - Math.Sqrt(x));
+                                //richTextBox1.AppendText(Convert.ToString(resCount) + '\n'); // вывод иксов 
+                                //x += h;
+                            }
+                            x += h;
+                        }
+                        res2 = res;
+                        n++;
+
+                        an = Math.Abs(res2 - res1);
+                        res1 = res;
+                    }
+                    while (an > eps);
+                    double Xg = a;
+
+
+                    double[] numX = new double[10];
+                    double[] numY = new double[10];
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        numX[i] = Math.Round(Xg, count);
+                        numY[i] = Math.Sqrt(400 - Math.Sqrt(Xg));
+                        Xg = Xg + h;
+                    }
+
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    chart1.Series[0].Points.DataBindXY(numX, numY);
+                    chart1.Series[0].LegendText = "√(400-√x)";
+                    label2.Text = Convert.ToString(Math.Round(res, count) + "   h=" + Math.Round(h, count));
+                    chart1.ChartAreas[0].AxisY.Minimum = 5; chart1.ChartAreas[0].AxisY.Maximum = 30;
+
+                }
+
+            }
+
+            catch
+            {
+                MessageBox.Show("Некоректные данные");
+            }
+
+
+
+
+
+            #region
+            //if ((textBox1.Text == "") || (textBox2.Text == "")  || (textBox3.Text == "") || (Convert.ToDouble(textBox1.Text) < 0) || (Convert.ToDouble(textBox1.Text) >100000) || (Convert.ToDouble(textBox2.Text) < 0) || (Convert.ToDouble(textBox2.Text) >100000) || (Convert.ToDouble(textBox3.Text) <0) || (Convert.ToDouble(textBox3.Text) >1) )
+            //{
+            //    MessageBox.Show("Некоректные данные");
+            //}
+            //else
+            //{
+            //    a = Convert.ToDouble(textBox1.Text);
+            //    b = Convert.ToDouble(textBox2.Text);
+            //    eps = Convert.ToDouble(textBox3.Text);
+            //    int count = GetDecimalDigitsCount(eps);
+
+            //    do
+            //    {
+            //        h = (b - a) / n;
+            //        label1.Text = Convert.ToString(Math.Round(h, count));
+            //        x = a; res = 0;
+            //        while (x + h <= b)
+            //        {
+            //            res += h * Math.Sqrt(400 - Math.Sqrt(x));
+            //            x += h;
+            //        }
+            //        res2 = res;
+            //        n++;
+
+            //        an = Math.Abs(res2 - res1);
+            //        res1 = res;
+            //    }
+            //    while (an > eps);
+            //    double Xg = a;
+
+
+            //    double[] numX = new double[10];
+            //    double[] numY = new double[10];
+
+            //    for(int i = 0; i < 10; i++)
+            //    {
+            //        numX[i] = Math.Round(Xg,count);
+            //        numY[i] = Math.Sqrt(400 - Math.Sqrt(Xg));
+            //        Xg = Xg + h;
+            //    }
+
+            //    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            //    chart1.Series[0].Points.DataBindXY(numX, numY);
+            //    chart1.Series[0].LegendText = "√(400-√x)";
+            //    label2.Text = Convert.ToString(Math.Round(res,count)+ "   h=" + Math.Round(h,count)); 
+            //    chart1.ChartAreas[0].AxisY.Minimum = 5; chart1.ChartAreas[0].AxisY.Maximum = 30;
+
+            //}
+            #endregion
+
+
+
+
+
+        }
+        static int GetDecimalDigitsCount(double number)
+        {
+            string str = number.ToString(new System.Globalization.NumberFormatInfo() { NumberDecimalSeparator = "." });
+            return str.Contains(".") ? str.Remove(0, Math.Truncate(number).ToString().Length + 1).Length : 0;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
